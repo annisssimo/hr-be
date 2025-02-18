088429dcd4b64938175d1abb82cedaf00bfa8a32
hanna-larchanka <hanna.larchanka@sunmait.tech>
2025-02-18 11:14:36 +0300

    fix: allow users to register names with ' (#17)

--- src/modules/endpoints/auth/register/register.schema.ts
+++ src/modules/endpoints/auth/register/register.schema.ts
@@ -6,16 +6,16 @@ export const RegisterSchema = z.object({
         .min(1)
         .max(11)
         .regex(
-            new RegExp('^[a-zA-Z]+$'),
-            'First name must contain only letters and have length between 1 and 11',
+            new RegExp("^[a-zA-Z']+$"),
+            'First name must contain only letters or apostrophes and have length between 1 and 11',
         ),
     lastName: z
         .string()
         .min(2)
         .max(15)
         .regex(
-            new RegExp('^[a-zA-Z]+$'),
-            'Last name must contain only letters and have length between 2 and 15',
+            new RegExp("^[a-zA-Z']+$"),
+            'Last name must contain only letters or apostrophes and have length between 2 and 15',
         ),
     email: z.string().email('Invalid email format').min(1),
     password: z
